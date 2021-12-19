0dc700849e9e98b20cf546f33bfd8cf5ff53d4f3
Unknown <luo14938@gmail.com>
2021-12-19 16:34:49 +0100

    adding endpoint /

--- dockerPrediction.py
+++ dockerPrediction.py
@@ -22,5 +22,8 @@ def root(Pregnancies: int = 6, Glucose: int = 148, BloodPressure: int = 72, Skin
         return ["We do not think you have Diabetes"]
 
 
+@app.get("/")
+def bla():
+    return ["Sry, try: ", "http://localhost:5000/calculate/?Pregnancies=1&Glucose=85&BloodPressure=66&SkinThickness=29&Insulin=0&BMI=26.6&DiabetesPedigreeFunctio=0.351&Age=31"]
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
